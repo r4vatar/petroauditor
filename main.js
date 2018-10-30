da52a334d2bd1f2c0cd489ef25acacb23dfa9d98
r4vatar <r4vatar@protonmail.com>
2018-10-30 18:39:29 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const Downloader = require('./lib/downloader.js');
+var download= new Downloader("https://explorador.petro.gob.ve/api");
+var argv = require('minimist')(process.argv.slice(2));
+
+if ((typeof argv.f)=="number") {
+  var from=argv.f;
+} else {
+  var from = 0;
+}
+
+if ((typeof argv.t)=="number") {
+  var to=argv.t;
+} else {
+  var to = from+1;
+}
+
+
+for(var i=from ; i<to; i++){
+  download.getblock(i).catch(error =>{
+    console.log(error)
+  })
+}
